4c4ef76e10c6225daadeec0fab475361ce3c2d3b
xunzhanggzl <1243286264@qq.com>
2019-08-14 20:12:50 +0800

    搭建添加信息接口

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -12,5 +12,25 @@ router.get('/test', (req, res) => {
   res.json({msg: "profile"})
 })
 
+// $route POST api/profiles/add
+// @desc  创建信息接口
+// @access Private
+router.post('/add', passport.authenticate('jwt', { session: false }),(req, res) => {
+  const profileFields = {};
+
+  if (req.body.type) profileFields.type = req.body.type;
+  if (req.body.describe) profileFields.describe = req.body.describe;
+  if (req.body.income) profileFields.income = req.body.income;
+  if (req.body.expend) profileFields.expend = req.body.expend;
+  if (req.body.cash) profileFields.cash = req.body.cash;
+  if (req.body.remark) profileFields.remark = req.body.remark;
+
+  new Profile(profileFields)
+    .save()
+    .then(profile => {
+      res.json(profile)
+    })
+})
+
 
 module.exports = router;
