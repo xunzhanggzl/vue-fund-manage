437e08688b6de7ffecdb0af00e57c1dfc464d274
xunzhanggzl <1243286264@qq.com>
2019-08-15 16:00:22 +0800

    路由守卫&token过期处理

--- client/src/http.js
+++ client/src/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 // 这是在.js文件中，需要引入，与 Register.vue 中的 this.$message 作对比
 import { Message,Loading } from 'element-ui';
+import router from './router'
+
 let loading;
 function startLoading() {
   loading = Loading.service({
@@ -18,6 +20,10 @@ function endLoading() {
 axios.interceptors.request.use(config => {
   // 加载动画
   startLoading();
+  if (localStorage.eleToken) {
+    // 设置统一的请求header
+    config.headers.Authorization = localStorage.eleToken;
+  }
   return config;
 }, error => {
   return Promise.reject(error);
@@ -31,6 +37,16 @@ axios.interceptors.response.use((response) => {
 }, (error) => {
   endLoading();
   Message.error(error.response.data);
+
+  // 获取错误状态码
+  const { status } = error.response;
+  if (status === 401) {
+    Message.error('token失效，请重新登录');
+    // 清除token
+    localStorage.removeItem('eleToken');
+    // 跳转到的登录页面
+    router.push('login');
+  }
   return Promise.reject(error);
 });
 
