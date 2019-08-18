ac425f623ae64686b068afbbcee2c8581bab1da2
xunzhanggzl <1243286264@qq.com>
2019-08-18 10:41:43 +0800

    左侧导航栏

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="index">
     <HeadNav></HeadNav>
+    <LeftMenu></LeftMenu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
   import HeadNav from '../components/HeadNav';
+  import LeftMenu from '../components/LeftMenu';
+
   export default {
     name: "index",
     components: {
-      HeadNav
+      HeadNav,
+      LeftMenu
     }
   }
 </script>
