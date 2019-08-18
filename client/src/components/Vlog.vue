653ef2416ac9eb2fcb67bf467352e92f277543d5
xunzhanggzl <1243286264@qq.com>
2019-08-18 15:20:31 +0800

    添加按钮

---
+++ client/src/components/Vlog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="">
+    <el-dialog
+      title="添加资金信息"
+      :visible.sync="vlog.show"
+      :close-on-click-modal=false
+      :modal-append-to-body=false
+      :close-on-press-escape=false
+    >
+      
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vlog',
+  data() {
+    return {
+      formData: {
+        type: "",
+        describe: "",
+        income: "",
+        expend: "",
+        cash: "",
+        remark: "",
+        id: ""
+      },
+      format_type_list:[
+        "提现",
+        "提现手续费",
+        "充值",
+        "优惠券",
+        "充值礼券",
+        "转账"
+      ],
+    }
+  },
+  props: {
+    vlog: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/FundList.vue
+++ client/src/views/FundList.vue
@@ -1,96 +1,112 @@
 <template>
   <div class="fillContainer">
-    <el-table
-      v-if="tableData.length > 0"
-      :data="tableData"
-      max-height="450"
-      border
-      style="width: 100%">
-      <el-table-column
-        type="index"
-        label="序号"
-        align='center'
-        width="70">
-      </el-table-column>
-      <el-table-column
-        prop="date"
-        label="创建时间"
-        align='center'
-        width="250">
-        <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span style="margin-left: 10px">{{ scope.row.date }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop='type'
-        label="收支类型"
-        align='center'
-        width="150">
-      </el-table-column>
-      <el-table-column
-        prop='describe'
-        label="收支描述"
-        align='center'
-        width="150">
-      </el-table-column>
-      <el-table-column
-        prop='income'
-        label="收入"
-        align='center'
-        width="100">
-        <template slot-scope="scope">
-          <span style="color:#00d053">{{ scope.row.income }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop='expend'
-        label="支出"
-        align='center'
-        width="100">
-        <template slot-scope="scope">
-          <span style="color:#f56767">{{ scope.row.expend }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop='cash'
-        label="账户现金"
-        align='center'
-        width="100">
-        <template slot-scope="scope">
-          <span style="color:#4db3ff">{{ scope.row.cash }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop='remark'
-        label="备注"
-        align='center'
-        width="200">
-      </el-table-column>
-      <el-table-column label="操作" align='center' prop="operation" width="180">
-        <template slot-scope="scope">
-          <el-button
-            type="warning"
-            icon="edit"
-            size="small"
-            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button
-            size="small"
-            type="danger"
-            icon="delete"
-            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="">
+      <el-form :inline="true" ref="add_data">
+        <el-form-item class="btnRight">
+          <el-button type="primary" size="small" icon="view" @click="handleAdd()">添加</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="tableContainer">
+      <el-table
+        v-if="tableData.length > 0"
+        :data="tableData"
+        max-height="450"
+        border
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          label="序号"
+          align='center'
+          width="70">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="创建时间"
+          align='center'
+          width="250">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop='type'
+          label="收支类型"
+          align='center'
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop='describe'
+          label="收支描述"
+          align='center'
+          width="150">
+        </el-table-column>
+        <el-table-column
+          prop='income'
+          label="收入"
+          align='center'
+          width="100">
+          <template slot-scope="scope">
+            <span style="color:#00d053">{{ scope.row.income }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop='expend'
+          label="支出"
+          align='center'
+          width="100">
+          <template slot-scope="scope">
+            <span style="color:#f56767">{{ scope.row.expend }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop='cash'
+          label="账户现金"
+          align='center'
+          width="100">
+          <template slot-scope="scope">
+            <span style="color:#4db3ff">{{ scope.row.cash }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop='remark'
+          label="备注"
+          align='center'
+          width="200">
+        </el-table-column>
+        <el-table-column label="操作" align='center' prop="operation" width="180">
+          <template slot-scope="scope">
+            <el-button
+              type="warning"
+              icon="edit"
+              size="small"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+              size="small"
+              type="danger"
+              icon="delete"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <Vlog :vlog="vlog"></Vlog>
   </div>
 </template>
 
 <script>
+  import Vlog from '../components/Vlog'
   export default {
     name: "fundlist",
     data() {
       return {
-        tableData: []
+        tableData: [],
+        vlog: {
+          show: false,
+          title: ""
+        }
       }
     }, 
     created() {
@@ -115,8 +131,14 @@
       handleDelete(index, row) {
         console.log(index);
         console.log(row);
+      },
+      handleAdd() {
+        this.vlog.show = true;
       }
     },
+    components: {
+      Vlog
+    }
   }
 </script>
 
